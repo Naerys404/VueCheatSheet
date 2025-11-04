a2188a7c7ae790df23b7b923622b53097a9b97b1
LauraG <naerys.404@gmail.com>
2025-11-04 16:56:41 +0100

    modif home

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -1,7 +1,21 @@
 <template>
-Page d'accueil
-</template>
+<div
+  class="hero min-h-screen"
+  style="background: linear-gradient( 98.7deg,  rgba(34,175,245,1) 2.8%, rgba(98,247,151,1) 97.8% );"
+>
 
+  <div class="hero-content text-neutral-content text-center">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-6xl font-bold">Bienvenue sur Vue CheatSheet</h1>
+      <p class="mb-5 text-lg">
+        Un petit site toolbox qui regroupe les cours et exercices sur VueJs vus avec <span class="text-primary text-bold">Adrar Pôle Numérique</span>
+      </p>
+      <button class="btn btn-primary mr-5">Voir les leçons</button>
+      <button class="btn btn-secondary">Voir les exercices</button>
+    </div>
+  </div>
+</div>
+</template>
 <style scoped>
 
 </style>
